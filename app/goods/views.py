378eb75f4777dfffe0ae22f79bd1b55e9b2b488b
joom <maks.kartakov16@gmail.com>
2024-05-13 21:03:27 +0700

    сделал пагинацию

--- app/goods/views.py
+++ app/goods/views.py
@@ -1,15 +1,23 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import get_list_or_404, render
 
 from goods.models import Products
 
 # Create your views here.
-def catalog(request):
+def catalog(request, category_slug, page=1):
     
-    goods = Products.objects.all()
-
+    if category_slug == 'all':
+        goods = Products.objects.all()
+    else:
+        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug)) 
+        
+    paginator = Paginator(goods, 3)
+    current_page= paginator.page(page)
+    
     content={
         'title': 'Аптека - Каталог',
-        'goods': goods,
+        'goods': current_page,
+        'slug_url': category_slug,
     }
     return render(request, 'goods/catalog.html', content)
 
