eba8de04ef06958ca8f562ee3f95b6100a48b32c
joom <maks.kartakov16@gmail.com>
2024-05-13 23:18:12 +0700

    добавил urlencode

--- app/goods/views.py
+++ app/goods/views.py
@@ -7,21 +7,28 @@ from goods.models import Products
 def catalog(request, category_slug):
     
     page = request.GET.get('page', 1)
+    on_sale= request.GET.get('on_sale', None)
+    order_by= request.GET.get('order_by', None)
 
     if category_slug == 'all':
         goods = Products.objects.all()
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug)) 
         
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+    if order_by and order_by != 'default':
+        goods = goods.order_by(order_by)
+    
     paginator = Paginator(goods, 3)
     current_page= paginator.page(int(page))
     
-    content={
+    context={
         'title': 'Аптека - Каталог',
         'goods': current_page,
         'slug_url': category_slug,
     }
-    return render(request, 'goods/catalog.html', content)
+    return render(request, 'goods/catalog.html', context)
 
 def product(request, product_slug):
     product = Products.objects.get(slug=product_slug)
