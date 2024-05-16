772d2c582f0c5b11bbd93bc62298b9b1ad0b15d8
joom <maks.kartakov16@gmail.com>
2024-05-17 01:17:59 +0700

    Решил проблему с фильтрами и корзиной

--- app/goods/views.py
+++ app/goods/views.py
@@ -2,14 +2,14 @@ from django.core.paginator import Paginator
 from django.shortcuts import get_list_or_404, render
 from goods.utils import q_search
 
-from goods.models import Products
+from goods.models import Products, Categories
 
 # Create your views here.
 def catalog(request, category_slug=None):
     
     page = request.GET.get('page', 1)
-    on_sale= request.GET.get('on_sale', None)
-    order_by= request.GET.get('order_by', None)
+    on_sale = request.GET.get('on_sale', None)
+    order_by = request.GET.get('order_by', None)
     query = request.GET.get('q', None)
 
     if category_slug == 'all':
@@ -17,20 +17,29 @@ def catalog(request, category_slug=None):
     elif query:
         goods = q_search(query)
     else:
-        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug)) 
+        try:
+            categories = Categories.objects.get(slug=category_slug)
+            goods = Products.objects.filter(category=categories)
+        except Categories.DoesNotExist:
+            goods = []  # Если категория не существует, возвращаем пустой список
         
     if on_sale:
-        goods = goods.filter(discount__gt=0)
+        goods = [good for good in goods if good.discount > 0]
     if order_by and order_by != 'default':
-        goods = goods.order_by(order_by)
+        if order_by == 'price':
+            goods = sorted(goods, key=lambda x: x.sell_price())
+        elif order_by == '-price':
+            goods = sorted(goods, key=lambda x: x.sell_price(), reverse=True)
     
     paginator = Paginator(goods, 3)
-    current_page= paginator.page(int(page))
+    current_page = paginator.page(int(page))
     
-    context={
+    context = {
         'title': 'Аптека - Каталог',
         'goods': current_page,
         'slug_url': category_slug,
+        'on_sale': on_sale,
+        'order_by': order_by,
     }
     return render(request, 'goods/catalog.html', context)
 
