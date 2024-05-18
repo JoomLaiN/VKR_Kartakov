351f8e304e25b7663a0f24f3b715d532386b59b3
joom <maks.kartakov16@gmail.com>
2024-05-19 01:58:46 +0700

    Начал делать образовательные элементы

--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,17 @@
 from typing import Mapping
 from django.http.response import HttpResponse
 from django.shortcuts import render
-
-from goods.models import Categories
+from goods.models import Products
+import random
 
 def index(request):
+    products_with_discount = Products.objects.filter(discount__gt=0)
+    
+    random_products = random.sample(list(products_with_discount), 3)
+    #random_products = 0
     context ={
         'title': 'Аптека - Главная',
+        'products': random_products,
     }
 
     return render(request, 'main/index.html', context)
