e80d4b3acac7485459603e2a989a95da01b2c1cc
JoomLaiN <99471301+JoomLaiN@users.noreply.github.com>
2024-06-17 02:33:00 +0700

    Update settings.py

--- app/karteka/settings.py
+++ app/karteka/settings.py
@@ -92,14 +92,18 @@ WSGI_APPLICATION = 'karteka.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'home',
+    #     'USER': 'home',
+    #     'PASSWORD': 'home',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'home',
-        'USER': 'home',
-        'PASSWORD': 'home',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        }
 }
 
 
